56e91fca3fe475a27681c0c965cec0c50b257be3
Akwilani <101393699+Akwilani@users.noreply.github.com>
2022-04-21 11:50:09 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * rev_string - reverse a string
- * @s:string
- * Return:void
- */
+* rev_string - reverse a string
+* @s:string
+* Return:void
+*/
 void rev_string(char *s)
 {
 int i, j;
