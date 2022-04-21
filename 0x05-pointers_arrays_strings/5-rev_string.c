52f41ff86314faf2cc45d0e13ae0ebc1dad7825b
Akwilani <101393699+Akwilani@users.noreply.github.com>
2022-04-21 11:43:48 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,17 @@ void rev_string(char *s)
 {
 int i, j;
 char a[500]
-i=0;
-j=0;
-        while (*(s+i))
+i = 0;
+j = 0;
+        while (*(s + i))
         {
-                *(a+i) = *(s+i);
+                *(a + i) = *(s + i);
                 i++;
         }
-        i = i-1;
+        i = i - 1;
         while (i >= 0)
         {
-                *(s+i) = *(a + j);
+                *(s + i) = *(a + j);
                 j++;
                 i--;
         }
