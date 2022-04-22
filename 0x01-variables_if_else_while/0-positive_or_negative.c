635973f90211d6be324db890179cb7275ba17bd6
Akwilani <101393699+Akwilani@users.noreply.github.com>
2022-04-22 08:21:40 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
 * main - Prints the numbers from 00 to 99
-*
 * Return: Always (Success)
 */
 int main(void)
@@ -10,17 +9,17 @@ int c, i;
 c = i = '0';
 for (c = '0'; c <= '9'; c++)
 {
-  for (i = '0'; i <= '9'; i++)
-  {
-    putchar(c);
-    putchar(i);
-    if ((c != '9') || (c == '9' && i != '9'))
-    {
-      putchar(',');
-      putchar(' ');
-    }
-  }
+for (i = '0'; i <= '9'; i++)
+{
+putchar(c);
+putchar(i);
+if ((c != '9') || (c == '9' && i != '9'))
+{
+putchar(',');
+putchar(' ');
 }
-  putchar('\n');
-  return (0);
+}
+}
+putchar('\n');
+return (0);
 }
