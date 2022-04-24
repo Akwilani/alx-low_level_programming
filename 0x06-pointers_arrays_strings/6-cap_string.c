d4785f6a9f405bb25d8916765a31fa9e712461ea
Akwilani <101393699+Akwilani@users.noreply.github.com>
2022-04-24 01:24:29 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 /**
  * cap_string - capitalizes all words of a string
  * @s: input string.
@@ -20,7 +20,7 @@ for (i = 0; i < 13; i++)
 {
 if (*(s + count) == separators[i])
 {
-if (((s + (count + 1)) >= 97) && ((s + (count + 1)) <= 122))
+if ((*(s + (count + 1)) >= 97) && ((s + (count + 1)) <= 122))
 *(s + (count + 1)) = *(s + (count + 1)) - 32;
 break;
 }
