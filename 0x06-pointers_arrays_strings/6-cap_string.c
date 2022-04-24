e2ef45cf2071f9b734a3a9c83c3de69b2cda6f2b
EmmA Odong <emmanuelodong87@gmail.com>
2022-04-24 01:50:56 -0700

    File six

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 /**
  * cap_string - capitalizes all words of a string
  * @s: input string.
@@ -6,7 +6,7 @@
  */
 
 
-char *cap_string(char *s)
+char *cap_string(char *)
 {
 int count = 0, i;
 int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
@@ -20,7 +20,7 @@ for (i = 0; i < 13; i++)
 {
 if (*(s + count) == separators[i])
 {
-if ((*(s + (count + 1)) >= 97) && ((s + (count + 1)) <= 122))
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 *(s + (count + 1)) = *(s + (count + 1)) - 32;
 break;
 }
